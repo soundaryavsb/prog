28e8fc18d56d9186fca80edf7ee5ce037934f457
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-07-11 06:21:30 -0700

    Create sam.c

---
+++ sam.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+int n,i,b,j,c[j];
+scanf("%d",&n);
+for(i=0;n>1;i++)
+{
+
+	b=n%2;
+//	printf("%d",b);
+            n=n/2;
+	for(j=0;;j++)
+	c[j]=b;
+}
+c[j]=n;
+int index=j;
+for(i=index;i>=0;i--)
+printf("%d",c[i]);
+	return 0;
+}
