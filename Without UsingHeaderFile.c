188d14175d1605a454517fe4e523c41e13a60fbf
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-07-28 22:21:51 -0700

    Create Without UsingHeaderFile.c

---
+++ Without UsingHeaderFile.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//int scanf(const int *format,...);
+int printf(const int *format,...);
+int num1=10,num2=20;
+int main(int num,char *argv[]) {
+//	int num1,num2;
+//	scanf("%d%d",&num1,&num2);
+	printf("%d\n",num1+num2);
+	printf("num=%d\n",num);
+	return 0;
+	// your code goes here
+}
